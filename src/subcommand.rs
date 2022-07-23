aa0f2510cabc70d64f51ac4d2ef5974ee36c5e14
NootNook <67638224+NootNook@users.noreply.github.com>
2022-07-23 20:49:29 +0200

    fix refactor format code

--- src/subcommand.rs
+++ src/subcommand.rs
@@ -2,47 +2,47 @@ use crate::utils::{get_provider, format_data, estimate_block_number_by_timestamp
 use ethers::providers::{Middleware, StreamExt};
 
 pub async fn live_pending_deploy_contract() -> Result<(), Box<dyn std::error::Error>> {
-	let provider = get_provider();
-	let mut stream_txs = provider
-		.watch_pending_transactions()
-		.await
-		.unwrap()
-		.stream();
+  let provider = get_provider();
+  let mut stream_txs = provider
+    .watch_pending_transactions()
+    .await
+    .unwrap()
+    .stream();
 
-	while let Some(tx_hash) = stream_txs.next().await {
-		if let Some(tx) = provider.get_transaction(tx_hash).await.unwrap() {
-			if tx.to == None {
-				// Creation contract
-				println!("{}", format_data(tx_hash));
-			}
-		}
-	}
+  while let Some(tx_hash) = stream_txs.next().await {
+    if let Some(tx) = provider.get_transaction(tx_hash).await.unwrap() {
+      if tx.to == None {
+        // Creation contract
+        println!("{}", format_data(tx_hash));
+      }
+    }
+  }
 
-	Ok(())
+  Ok(())
 }
 
 pub async fn history_deploy_contract(start_timestamp: u64, is_progress_status: bool) -> Result<(), Box<dyn std::error::Error>> {
-	let provider = get_provider();
-	let latest_block = provider.get_block_number().await.unwrap().as_u64();
-	let start_block = estimate_block_number_by_timestamp(start_timestamp, latest_block).await;
+  let provider = get_provider();
+  let latest_block = provider.get_block_number().await.unwrap().as_u64();
+  let start_block = estimate_block_number_by_timestamp(start_timestamp, latest_block).await;
 
-	let diff_block = latest_block - start_block;
+  let diff_block = latest_block - start_block;
 
-	for current_block in start_block..latest_block {
-		if let Some(block) = provider.get_block_with_txs(current_block).await.unwrap() {
-			let current_progress = current_block - start_block;
-			for tx in block.transactions.iter() {
-				if tx.to == None {
-					if is_progress_status {
-						let progression = (current_progress as f64/ diff_block as f64) * 100 as f64;
-						println!("{} - Progress status : {:.2} %", format_data(tx.hash), progression);
-					} else {
-						println!("{}", format_data(tx.hash));
-					}
-				}
-			}
-		}
-	}
+  for current_block in start_block..latest_block {
+    if let Some(block) = provider.get_block_with_txs(current_block).await.unwrap() {
+      let current_progress = current_block - start_block;
+      for tx in block.transactions.iter() {
+        if tx.to == None {
+          if is_progress_status {
+            let progression = (current_progress as f64/ diff_block as f64) * 100 as f64;
+            println!("{} - Progress status : {:.2} %", format_data(tx.hash), progression);
+          } else {
+            println!("{}", format_data(tx.hash));
+          }
+        }
+      }
+    }
+  }
 
-	Ok(())
+  Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -6,34 +6,34 @@ use once_cell::sync::{OnceCell};
 const RPC: &str = env!("RPC_HTTPS_ETH");
 
 fn get_instance_provider() -> &'static Mutex<Provider<Http>> {
-	static INSTANCE: OnceCell<Mutex<Provider<Http>>> = OnceCell::new();
-	INSTANCE.get_or_init(|| {
-		let m = Provider::<Http>::try_from(RPC).unwrap();
-		Mutex::new(m)
-	})
+  static INSTANCE: OnceCell<Mutex<Provider<Http>>> = OnceCell::new();
+  INSTANCE.get_or_init(|| {
+    let m = Provider::<Http>::try_from(RPC).unwrap();
+    Mutex::new(m)
+  })
 }
 
 pub fn get_provider() -> Provider<Http> {
-	get_instance_provider().lock().unwrap().clone()
+  get_instance_provider().lock().unwrap().clone()
 }
 
 pub fn format_data(hash: H256) -> String {
-	let now: DateTime<Utc> = Utc::now();
-	format!("{}\nhttps://etherscan.io/tx/{:#x}", now, hash)
+  let now: DateTime<Utc> = Utc::now();
+  format!("{}\nhttps://etherscan.io/tx/{:#x}", now, hash)
 }
 
 pub fn get_timestamp_on_block(block: Block<H256>) -> u64 {
-	block.timestamp.as_u64()
+  block.timestamp.as_u64()
 }
 
 // Multiple ways, binary search...
 //I choose a big approximation
 pub async fn estimate_block_number_by_timestamp(start_timestamp: u64, latest_block: u64) -> u64 {
-	const AVERAGE_MINING_TIME: u64 = 13; // https://ycharts.com/indicators/ethereum_average_block_time
+  const AVERAGE_MINING_TIME: u64 = 13; // https://ycharts.com/indicators/ethereum_average_block_time
 
-	let provider = get_provider();
-	let data_latest_block = provider.get_block(latest_block).await.unwrap().unwrap();
-	let timestamp_latest = get_timestamp_on_block(data_latest_block);
+  let provider = get_provider();
+  let data_latest_block = provider.get_block(latest_block).await.unwrap().unwrap();
+  let timestamp_latest = get_timestamp_on_block(data_latest_block);
 
-	latest_block - ((timestamp_latest - start_timestamp) / AVERAGE_MINING_TIME)
+  latest_block - ((timestamp_latest - start_timestamp) / AVERAGE_MINING_TIME)
 }
